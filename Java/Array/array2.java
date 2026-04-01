d79b041923d2a042fbc04ea42390c7cfa320b6b5
Aman Shaikh <amanshaikh9156@gmail.com>
2026-04-01 12:28:45 +0530

    Initial commit

---
+++ Java/Array/array2.java
@@ -0,0 +1,16 @@
+package Array;
+public class array2 {
+    public static void main(String[] args) {
+    int[] ages = {20,50,45,30,19,48,75,35,64,24,26,37};
+
+    float avg , sum = 0;
+    int length = ages.length;
+
+    for( int age : ages){
+        sum += age;
+    }
+    avg = sum/length;
+
+    System.out.println(avg);
+    }
+}
