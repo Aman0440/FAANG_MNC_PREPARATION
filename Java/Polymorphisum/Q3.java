d465fbb11082ffb4b0bcd001079b7c9eebc10ae6
Aman Shaikh <amanshaikh9156@gmail.com>
2026-04-06 03:19:08 +0530

    day 4 practice quesition

--- Java/Polymorphisum/Q3.java
+++ Java/Polymorphisum/Q3.java
@@ -1,5 +1,38 @@
 package Polymorphisum;
+// Q3
 
+// Create:
+
+// Shape
+// Circle
+// Rectangle
+
+// Override area()
+
+class Shape{
+    void area(){
+        System.out.println("Area");
+    }
+}
+class Circle extends Shape{
+    @Override
+    void area(){
+        System.out.println("Area of Circle");
+    }
+}
+class Rectangle extends Shape{
+    @Override
+    void area(){
+        System.out.println("Area of Rectangle");
+    }
+}
 public class Q3 {
+    public static void main(String[] args){
+        Shape s1 = new Circle();
+        Shape s2 = new Rectangle();
+
+        s1.area();
+        s2.area();
+    }
     
 }
