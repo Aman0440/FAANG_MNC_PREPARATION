a9a3f9afc83a78503f821a26e992ab21bdf282b8
Aman Shaikh <amanshaikh9156@gmail.com>
2026-04-07 23:53:35 +0530

    strated exception handling

---
+++ Java/ExceptionHandling/Q1.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class Q1{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+    }
+}
